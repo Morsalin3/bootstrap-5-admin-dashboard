a2857bc871c5714d56b0a9530ddbef09f7fd6260
Morsalin3 <mdmorsalin3@gmail.com>
2025-08-21 11:21:41 +0600

    update navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const sidebarToggle = document.querySelector("#sidebar-toggle");
-sidebarToggle.addEventListener("click", function(){
+    sidebarToggle.addEventListener("click", function(){
     document.querySelector("#sidebar").classList.toggle("collapsed")
+
 });
 
 document.querySelector(".theme-toggle").addEventListener("click",()=>{
